4b5c90e0c536d5a160fc0df3491ea31c2092759f
Michael Roose <mjroose@gmail.com>
2018-10-28 14:23:42 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,12 +8,12 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
-
-  end
-
   get '/posts/new' do
     erb :new
+  end
+
+  get '/posts' do
+    erb :index
   end
 
   post '/posts' do
